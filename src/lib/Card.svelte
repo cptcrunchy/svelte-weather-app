36844cbd1dc7c7d522f87287b6fbd5122a3e975f
Jason Gutierrez <jasong84@gmail.com>
2021-04-17 17:21:04 -0500

    Populate Card component with forecast information

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,35 +1,46 @@
+<script>
+    export let forecast = null;
+    export let locationInfo = null;
+
+    let isMetric = false;
+    
+</script>
+
+
+
 <div class="flex flex-col bg-gray-100 rounded p-4 w-full max-w-xs shadow-lg">
-    <div class="font-bold text-xl">Sydney</div>
-    <div class="text-sm text-gray-500">Thursday 10 May 2020</div>
+    <div class="font-bold text-xl">{locationInfo.name}, {locationInfo.region}</div>
+
+    <div class="text-sm text-gray-500">{new Date(forecast.date).toDateString()}</div>
     <div class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-<svg class="w-32 h-32" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"></path></svg>
+        <img src={forecast.day.condition.icon} alt={forecast.day.condition.text} />
     </div>
     <div class="flex flex-row items-center justify-center mt-6">
-        <div class="font-medium text-6xl">24°</div>
+        <div class="font-medium text-6xl">{isMetric ? Math.floor(forecast.day.avgtemp_c) : Math.floor(forecast.day.avgtemp_f)}&deg;</div>
         <div class="flex flex-col items-center ml-6">
-            <div>Cloudy</div>
+            <div>{forecast.day.condition.text}</div>
             <div class="mt-1">
                 <span class="text-sm"><i class="far fa-long-arrow-up"></i></span>
-                <span class="text-sm font-light text-gray-500">28°C</span>
+                <span class="text-sm font-light text-gray-500">High: {isMetric ? Math.floor(forecast.day.maxtemp_c) : Math.floor(forecast.day.maxtemp_f)}&deg;{isMetric ? "C" : "F"}</span>
             </div>
             <div>
                 <span class="text-sm"><i class="far fa-long-arrow-down"></i></span>
-                <span class="text-sm font-light text-gray-500">20°C</span>
+                <span class="text-sm font-light text-gray-500">Low: {isMetric ? Math.floor(forecast.day.mintemp_c) : Math.floor(forecast.day.mintemp_f)}&deg;{isMetric ? "C" : "F"}</span>
             </div>
         </div>
     </div>
     <div class="flex flex-row justify-between mt-6">
         <div class="flex flex-col items-center">
             <div class="font-medium text-sm">Wind</div>
-            <div class="text-sm text-gray-500">9k/h</div>
+            <div class="text-sm text-gray-500">{forecast.day.maxwind_kph}k/h</div>
         </div>
         <div class="flex flex-col items-center">
             <div class="font-medium text-sm">Humidity</div>
-            <div class="text-sm text-gray-500">68%</div>
+            <div class="text-sm text-gray-500">{forecast.day.avghumidity}%</div>
         </div>
         <div class="flex flex-col items-center">
             <div class="font-medium text-sm">Visibility</div>
-            <div class="text-sm text-gray-500">10km</div>
+            <div class="text-sm text-gray-500">{forecast.day.avgvis_km}km</div>
         </div>
     </div>
 </div>
