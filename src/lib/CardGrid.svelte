65b5e97d577d394354b0eeeea88832f45113996e
Jason Gutierrez <jasong84@gmail.com>
2021-04-17 17:21:26 -0500

    Layout grid for each card

--- src/lib/CardGrid.svelte
+++ src/lib/CardGrid.svelte
@@ -1,15 +1,15 @@
 <script>
-    import forecastStore from '../data/forecast-store'
     import Card from '$lib/Card.svelte';
-
-    $:console.log($forecastStore)
-
+    import forecastStore from '../data/forecast-store'   
 </script>
 
 
 <div class="flex sm:flex-row flex-col gap-y-4 gap-x-4 mt-6 justify-center items-center sm:items-start">
-    {#each [1,2,3,4,5] as id}
-	    <Card />
-    {/each}
-	
+    {#if $forecastStore.forecast}
+        {#each $forecastStore.forecast.forecastday as forecast}
+            <Card {forecast} locationInfo={$forecastStore.location} />
+        {/each}
+    {:else}
+        <p>Search for weather in your area</p>
+    {/if}
 </div>
